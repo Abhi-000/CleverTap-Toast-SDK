af2113cdd3c6b227214d6e5ed1c310dbc2acca9d
Abhishek Shah <abhishekhshah2001@gmail.com>
2025-02-04 23:50:31 +0530

    initial commit

---
+++ Assets/Samples~/DemoScene/Scripts/ToastDemoController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using CleverTap.Notifications;
+
+public class ToastDemoController : MonoBehaviour
+{
+    public void ShowShortToast()
+    {
+        ToastNotificationManager.Instance.ShowToast("Short Toast Message");
+    }
+
+    public void ShowLongToast()
+    {
+        ToastNotificationManager.Instance.ShowToast("Long Toast Message", ToastNotificationManager.ToastDuration.Long);
+    }
+
+    public void ShowErrorToast()
+    {
+        ToastNotificationManager.Instance.ShowToast("Error Occurred!", ToastNotificationManager.ToastDuration.Long);
+    }
+}
